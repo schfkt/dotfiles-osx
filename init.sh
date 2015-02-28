840fba339a0080d980246bb7acd8be0e45bb5456
Pavel Ivanov <ivpavig@gmail.com>
2015-02-28 19:02:19 +0300

    Remove linux-specific stuff

--- init.sh
+++ init.sh
@@ -8,18 +8,7 @@ cd $DOTFILES_DIR
 git submodule init
 git submodule update
 
-# konsole
-ln -s ~/dotfiles/konsole/profiles ~/.kde/share/apps/konsole
-ln -s ~/dotfiles/konsole/konsolerc ~/.kde/share/config/konsolerc
-
 # other configs
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/i3 ~/.i3
-ln -s ~/dotfiles/xsessionrc ~/.xsessionrc
 
-# binaries
-mkdir -p ~/bin
-for file in "$DOTFILES_DIR/bin/*"; do
-  ln -s $file ~/bin/
-done
